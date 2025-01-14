a6bee93db6820b2ab455f864938bd487e8d7798c
tomfyfe <tomfyfe85@gmail.com>
2025-01-14 14:22:05 +0000

    customizing the Admin

---
+++ movies/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Genre, Movie
+
+class GenreAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+class MovieAdmin(admin.ModelAdmin):
+	exclude = ('date_created',)
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
+
