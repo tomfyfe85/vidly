1cb0b65f6320ce19effeb9fc5a119be8e795caef
tomfyfe <tomfyfe85@gmail.com>
2025-01-14 22:28:03 +0000

    templates complete

--- movies/admin.py
+++ movies/admin.py
@@ -1,12 +1,19 @@
+"""
+Admin configuration for the movies app.
+This module defines custom admin classes for the Genre and Movie models 
+to customize their appearance and behavior in the Django admin interface.
+"""
+
 from django.contrib import admin
 from .models import Genre, Movie
 
 class GenreAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name')
+    """Configures the Genre model in the admin with specific list displays."""
+    list_display = ('id', 'name')
 
 class MovieAdmin(admin.ModelAdmin):
-	exclude = ('date_created',)
+    """Configures the Movie model in the admin with excluded fields."""
+    exclude = ('date_created',)
 
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Movie, MovieAdmin)
-
