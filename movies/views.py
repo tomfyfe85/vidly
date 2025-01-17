039930fe472eafe10f95803fa92dba14be314cc6
tomfyfe <tomfyfe85@gmail.com>
2025-01-17 14:24:12 +0000

    error handling complete

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,7 @@
 from platform import release
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 # Create your views here.
@@ -11,5 +11,5 @@ def index(request):
 	return render(request, 'movies/index.html', {'movies': movies}) 
 
 def detail(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
-    return render(request, 'movies/detail.html', {'movie': movie}) 
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
